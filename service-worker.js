61cd6b7d142f9696a55218a111edea9bdbaa9f39
witnese-sudo <witnese@gmail.com>
2026-05-07 04:29:13 +0200

    Implement service worker for caching assets

---
+++ service-worker.js
@@ -0,0 +1,37 @@
+const CACHE_NAME = "spacefrogs-pwa-v0-1";
+
+const FILES_TO_CACHE = [
+  "./",
+  "index.html",
+  "css/main.css",
+  "js/main.js",
+  "manifest.json"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(FILES_TO_CACHE);
+    })
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys
+          .filter((key) => key !== CACHE_NAME)
+          .map((key) => caches.delete(key))
+      );
+    })
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) => {
+      return cachedResponse || fetch(event.request);
+    })
+  );
+});
